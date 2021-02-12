4bdf494ac097b90c2c0ccccfbd73f8b6a53d0a71
Steven Shi <48036250+yukaishi@users.noreply.github.com>
2021-02-12 09:33:17 -0800

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+
+print("Hello world! - from Steven Shi")
+print("Current Time =", current_time)
+
